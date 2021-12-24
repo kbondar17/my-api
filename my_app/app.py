cb819013c549d8e998fae7248a488d58fc06464b
kbondar17 <ironbondar@gmail.com>
2021-12-24 14:51:44 +0300

    !

--- my_app/app.py
+++ my_app/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request
-from my_app.bd.models import db
 # from bd import db_funs
 import logging
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_pyfile('bd/config.py')
-    db.init_app(app)
+
 
     @app.route('/add', methods=['PUT'])
     def add_news():
