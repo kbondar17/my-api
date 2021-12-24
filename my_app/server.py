f0845906c2ab32ccae7a2a475375b6d74a3fe36b
kbondar17 <ironbondar@gmail.com>
2021-12-24 11:26:54 +0300

    !

---
+++ my_app/server.py
@@ -0,0 +1,27 @@
+# from flask import Flask, request
+# from my_app.bd.models import db
+# from my_app.bd import db_funs
+#
+# def create_app():
+#     app = Flask(__name__)
+#     app.config.from_pyfile('bd/config.py')
+#     db.init_app(app)
+#
+#     @app.route('/add', methods=['PUT'])
+#     def add_news():
+#         print('-- вошли в add_news')
+#         print('-- вот request.json:\n',request.json)
+#         return f'вот твой кал --- {request.json}'
+#
+#
+#     @app.route("/")
+#     def hello():
+#         return "Привет!"
+#
+#
+#     return app
+#
+#
+# if __name__=="__main__":
+#     app = create_app()
+#     app.run(debug=True)
--- my_app/wsgi.py
+++ my_app/wsgi.py
@@ -1,4 +1,4 @@
-from app import app
+from my_app.app import app
 
 if __name__ == '__main__':
     print('стартуем в wsgi')
